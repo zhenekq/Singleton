28f611958adb9df715ae9a7518e7d919b13b1d01
zhenekq <zhenek02ss@gmail.com>
2021-04-02 15:16:48 +0300

    first meeting with Singleton

--- src/by/epamtc/other/Singleton.java
+++ src/by/epamtc/other/Singleton.java
@@ -1,5 +1,22 @@
 package by.epamtc.other;
 
 public class Singleton {
+    private static Singleton programLogger;
+    private static String logFile = "This is log file. \n\n";
 
+    public static synchronized Singleton getProgramLogger(){
+        if (programLogger == null){
+            programLogger = new Singleton();
+        }
+        return programLogger;
+    }
+    private Singleton(){
+
+    }
+    public void addLogInfo(String logInfo){
+        logFile += logInfo + "\n";
+    }
+    public void showLogFile(){
+        System.out.println(logFile);
+    }
 }
--- src/by/epamtc/runner/Main.java
+++ src/by/epamtc/runner/Main.java
@@ -1,7 +1,13 @@
 package by.epamtc.runner;
 
+import by.epamtc.other.Singleton;
+
 public class Main {
     public static void main(String[] args) {
+        Singleton.getProgramLogger().addLogInfo("First log...");
+        Singleton.getProgramLogger().addLogInfo("Second log..");
+        Singleton.getProgramLogger().addLogInfo("Third log log...");
 
+        Singleton.getProgramLogger().showLogFile();
     }
 }
